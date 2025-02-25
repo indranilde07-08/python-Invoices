00d859c2da2e260e10e1ebec28aaceac6df6304e
indranild de <84306058+indranil07-8@users.noreply.github.com>
2025-02-25 15:50:40 +0530

    Initial commit

---
+++ function.py
@@ -0,0 +1,15 @@
+def get_todos(filepath="todos.txt"):
+    with open(filepath, 'r') as file_name:
+        """ Read a text file and return the list of to-do items. """
+        todos_local = file_name.readlines()
+        return todos_local
+
+
+def write_todos(todos_arg, filepath="todos.txt"):
+    """Write the to-do items list in the text file."""
+    with open(filepath, 'w') as file_name:
+        file_name.writelines(todos_arg)
+
+if __name__ == "__main__":
+    print("hello")
+    print(get_todos())
