d3c42b98d7406bd1becc976bdff767fca0788eba
indranild de <84306058+indranil07-8@users.noreply.github.com>
2025-02-28 13:16:13 +0530

    completed to-do app

---
+++ Todo_web-app/web.py
@@ -0,0 +1,32 @@
+import streamlit as st
+import function
+
+todos = function.get_todos()
+
+
+def add_todo():
+    todo = st.session_state["add"]+"\n"
+    todos.append(todo)
+    function.write_todos(todos)
+    st.session_state["add"]= ""
+
+
+st.title("My Todo App")
+st.subheader("This is my todo app")
+st.write("This app is increase your productivity")
+
+for index,todo in enumerate(todos):
+    checkbox = st.checkbox(todo,key=f"todo_{index}")
+    if checkbox:
+        todos.pop(index)
+        function.write_todos(todos)
+        del st.session_state[f"todo_{index}"]
+        st.rerun() #instant refresh
+
+st.text_input(label= "", placeholder="Add new todo....",on_change=add_todo,
+              key="add")
+
+
+
+
+
