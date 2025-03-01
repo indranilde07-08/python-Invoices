b8881a73ee1d8175e92dcf75439300cb5a87f963
indranild de <84306058+indranil07-8@users.noreply.github.com>
2025-03-01 19:21:33 +0530

    Create columns,image, and description

--- My portfolio/main.py
+++ My portfolio/main.py
@@ -1,16 +1,17 @@
-# This is a sample Python script.
+import streamlit as st
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+st.set_page_config(layout="wide")#wide on your website
+col1 , col2 = st.columns(2)
 
+with col1:
+    st.image("images/481217973_1842540879848818_8357691637776820431_n.png",width= 500)
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+with col2:
+    st.title("Indranil de")
+    content = """
+    Hi, I am Indranil De. I graduated in 2024 in Electronic and Communication Engineering. 
+    I am very interested in Python programming, automation, data processing, and data analysis. 
+    This portfolio shows my skills 
+    and what I have built in projects.
+    """
+    st.info(content)
