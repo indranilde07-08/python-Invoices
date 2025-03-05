81e6f1bd9f0b9a5d299aeca9b18efab2cc2ad4bb
indranild de <84306058+indranil07-8@users.noreply.github.com>
2025-03-05 12:55:39 +0530

    Add app description,update new url in data_set,add app  image and links

--- My portfolio/main.py
+++ My portfolio/main.py
@@ -22,14 +22,20 @@ content2 = """
            """
 st.subheader(content2)
 
-col3,col4 = st.columns(2)
+col3, empty_column, col4 = st.columns([2,1,2])
 
-df = pd.read_csv("data.csv",sep=";")
+df = pd.read_csv("data.csv", sep=";")
 with col3:
     for index,item in df[:10].iterrows(): #Iterates over DataFrame rows
         st.header(item["title"])#Gets the title for that row
+        st.write(item["description"])
+        st.image("images/"+item["image"])
+        st.write(f"[Source Code]({item['url']})")
 
 
 with col4:
     for index,item in df[10:].iterrows():
         st.header(item["title"])
+        st.write(item["description"])
+        st.image("images/"+item["image"])
+        st.write(f"[Source Code]({item['url']})")
