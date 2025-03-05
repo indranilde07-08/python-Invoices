a61281e3690ef1665ccc34f3b93dd7feb909e592
indranild de <84306058+indranil07-8@users.noreply.github.com>
2025-03-05 20:42:23 +0530

    Send email using the form

--- My portfolio/pages/Contact.py
+++ My portfolio/pages/Contact.py
@@ -1,8 +1,21 @@
 import streamlit as st
+from send_email import send_email
 
 st.title("Contact Me")
 with st.form(key="email_items"):
     user_email = st.text_input("Your email address")
-    message = st.text_area("Your message")
+    raw_message = st.text_area("Your message")
+    message = f"""
+    Subject: New email from{user_email}
+    From: {user_email}
+    {raw_message}
+    
+
+"""
     button = st.form_submit_button("submit")
+
+
+    if button:
+        send_email(message, user_email)
+        st.info("Your email was send successfully")
 
---
+++ My portfolio/send_email.py
@@ -0,0 +1,22 @@
+import smtplib  # This module defines an SMTP (Simple Mail Transfer Protocol)
+# client session,
+# which is used to send emails
+import ssl  # This module provides secure (TLS/SSL) connections,
+
+
+# which are needed for securely connecting to email servers.
+
+def send_email(message, user_email):
+    host = "smtp.gmail.com"
+    port = 465
+    username = "indranilde92@gmail.com"
+    password = "wkjj yqdk ymcx hvop"
+    receiver = user_email
+    context = ssl.create_default_context()  # Creates a secure SSL context for
+    # encrypting the connection between your Python
+    # script and the Gmail server.
+
+    with smtplib.SMTP_SSL(host=host, port=port,
+                          context=context) as server:  # Establishes a secure connection to the Gmail SMTP server using SSL.
+        server.login(username, password)
+        server.sendmail(username, receiver, message)
