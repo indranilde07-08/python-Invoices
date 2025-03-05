b7046ec55f81a4c9fefa138e8afebdf0b331cfa2
indranild de <84306058+indranil07-8@users.noreply.github.com>
2025-03-05 20:09:57 +0530

    Send email

--- My portfolio/pages/Contact.py
+++ My portfolio/pages/Contact.py
@@ -1,1 +1,8 @@
+import streamlit as st
+
+st.title("Contact Me")
+with st.form(key="email_items"):
+    user_email = st.text_input("Your email address")
+    message = st.text_area("Your message")
+    button = st.form_submit_button("submit")
 
